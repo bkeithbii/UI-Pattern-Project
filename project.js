43090dbfd9e45ff28d3e8064f9487ebd0d138e5e
Brian Brown II <bkeithbii@gmail.com>
2020-02-07 14:55:28 -0500

    begin defining variables in JS

---
+++ project.js
@@ -0,0 +1,7 @@
+//Define the slideshow elements
+const slideshowSlide = document.querySelector(".slideshow-slide");
+const slideshowImages = document.querySelector(".slideshow-slide img");
+
+//Define buttons using JS
+const prevButt = document.querySelector("#prevButt");
+const nextButt = document.querySelector("#nextButt");
