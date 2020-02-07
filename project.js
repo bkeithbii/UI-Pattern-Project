810f45cdf76a47a642c538c4bdc30a7ceed868e6
Brian Brown II <bkeithbii@gmail.com>
2020-02-07 15:45:52 -0500

    adjusting button functionality

--- project.js
+++ project.js
@@ -1,7 +1,26 @@
 //Define the slideshow elements
 const slideshowSlide = document.querySelector(".slideshow-slide");
-const slideshowImages = document.querySelector(".slideshow-slide img");
+const slideshowImages = document.querySelectorAll(".slideshow-slide img");
 
 //Define buttons using JS
 const prevButt = document.querySelector("#prevButt");
 const nextButt = document.querySelector("#nextButt");
+
+//Make a variable to keep track of place in slideshow
+let counter = 1;
+const size = slideshowImages[0].clientWidth;
+
+slideshowSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+//Add event listeners for buttons
+nextButt.addEventListener("click", function(e) {
+  slideshowSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  slideshowSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+prevButt.addEventListener("click", function(e) {
+  slideshowSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  slideshowSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
